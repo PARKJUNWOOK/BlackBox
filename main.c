1fa0a5a387843b863a016db7576cedaeb24c8a4d
BookWho <50477988+PARKJUNWOOK@users.noreply.github.com>
2019-07-11 14:09:02 +0900

    Update main.c

--- main.c
+++ main.c
@@ -18,33 +18,32 @@ int main()
     /*
         ************************************
         1. 마운트 디바이스 검사 (연결체크, 용량체크)
-        - 별도 쓰레드로 러닝하는 방법
-        - 쓰레드로인하여 전체프로세스가 죽는것을 방지하기위해서는 삭제.c를 따로 배치러닝시킨다
+        - 별도 쓰레드로 러닝
         ************************************
      */
-    MOUNTP *mntSize = getDirSize();     
-    printf("현재 마운트 드라이브의 사용량은 %d %% 입니다.\n", mntSize->size.percent);
+    // MOUNTP *mntSize = getDirSize();     
+    // printf("현재 마운트 드라이브의 사용량은 %d %% 입니다.\n", mntSize->size.percent);
 
-    // 마운트 드라이버의 유휴공간이 10% 이하일 경우 오래된 영상폴더&파일 삭제처리
-    if(mntSize->size.percent < 10)
-    {
-        printf("ROOT_FOLDER_PATH :: %s\n", ROOT_FOLDER_PATH);
-        printf("ROOT_FOLDER_PATH2 :: %s\n", ROOT_FOLDER_PATH2);
+    // // 마운트 드라이버의 유휴공간이 10% 이하일 경우 오래된 영상폴더&파일 삭제처리
+    // if(mntSize->size.percent < 10)
+    // {
+    //     printf("ROOT_FOLDER_PATH :: %s\n", ROOT_FOLDER_PATH);
+    //     printf("ROOT_FOLDER_PATH2 :: %s\n", ROOT_FOLDER_PATH2);
         
-        char *oldDir; 
-        oldDir = searchOldDir(ROOT_FOLDER_PATH2);
+    //     char *oldDir; 
+    //     oldDir = searchOldDir(ROOT_FOLDER_PATH2);
         
-        printf("##### 삭제대상 디렉토리명 :: %s\n", oldDir);
+    //     printf("##### 삭제대상 디렉토리명 :: %s\n", oldDir);
 
-        printf("##### 드라이브 내 용량이 부족하여 삭제 프로세스를 수행합니다\n");
-        printf("##### 삭제 대상 폴더경로 :: %s\n", oldDir);
+    //     printf("##### 드라이브 내 용량이 부족하여 삭제 프로세스를 수행합니다\n");
+    //     printf("##### 삭제 대상 폴더경로 :: %s\n", oldDir);
         
-        rmdirs(oldDir, 1);
+    //     rmdirs(oldDir, 1);
 
-        printf("##### %s 하위 파일 및 폴더삭제를 완료 하였습니다.\n", oldDir);
-    }
-    else
-        printf("현재 마운트 드라이브의 유휴공간이 10 %% 이상입니다!!\n");
+    //     printf("##### %s 하위 파일 및 폴더삭제를 완료 하였습니다.\n", oldDir);
+    // }
+    // else
+    //     printf("현재 마운트 드라이브의 유휴공간이 10 %% 이상입니다!!\n");
     
     /*
         ************************************
@@ -67,18 +66,18 @@ int main()
                 * DELETE USED DIR 190702
             */
 
-            printf("##### This Folder Used... delete Folder and new Create Folder \n");
+            // printf("##### This Folder Used... delete Folder and new Create Folder \n");
             
-            // folder delete and new Create Folder
-            int chkFlag = 0;
-            chkFlag = delDirFile(currTime);
-            printf("chkFlag :: %d\n", chkFlag);
+            // // folder delete and new Create Folder
+            // int chkFlag = 0;
+            // chkFlag = delDirFile(currTime);
+            // printf("chkFlag :: %d\n", chkFlag);
 
-            if(chkFlag != 1)
-            {
-                printf("##### Dir Delete Error!! \n");
-                exit(0);
-            }
+            // if(chkFlag != 1)
+            // {
+            //     printf("##### Dir Delete Error!! \n");
+            //     exit(0);
+            // }
         }
         else        // 폴더 생성 성공 시
         {
@@ -96,15 +95,15 @@ int main()
            int retCode;
            do
            {
-                // int ch;  
-                // for(; !(ch=='\n');){  
-                //     ch = getch();  
-                //     printf("키 입력을 감지했습니다. 프로그램을 종료합니다. \n", ch);
-                //     exit(0);
-                // }
+                int ch;  
+                for(; !(ch=='\n');){  
+                    ch = getch();  
+                    printf("키 입력을 감지했습니다. 프로그램을 종료합니다. \n", ch);
+                    exit(0);
+                }
 
                 printf("##### %d 회차 녹화를 시작합니다. #####\n", count);
-                retCode = system("python /home/nvidia/test/rec_video.py");
+                retCode = system("python /home/jk/workspace/blackbox/hello.py");
                 if(retCode != 0)
                 {
                     perror(".py systemCall Error!!");
