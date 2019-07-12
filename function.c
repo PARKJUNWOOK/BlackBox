0a7ec6a9122c7957b33a68978ae47918def26c8c
BookWho <50477988+PARKJUNWOOK@users.noreply.github.com>
2019-07-12 15:35:04 +0900

    Update function.c
    소스 수정 thread 버전 반영

--- function.c
+++ function.c
@@ -511,3 +511,39 @@ int getch(void)
 
     return ch;  
 }
+
+/*
+    * 유휴공간 확보를 위한 삭제 쓰레드 펑션
+ */
+void deleteProc(void* data)
+{
+    while(1)
+    {
+        MOUNTP *mntSize = getDirSize();     
+        printf("현재 마운트 드라이브의 사용량은 %d %% 입니다.\n", mntSize->size.percent);
+
+        // 마운트 드라이버의 유휴공간이 10% 이하일 경우 오래된 영상폴더&파일 삭제처리
+        if(mntSize->size.percent < 10)
+        {
+            printf("ROOT_FOLDER_PATH :: %s\n", ROOT_FOLDER_PATH);
+            printf("ROOT_FOLDER_PATH2 :: %s\n", ROOT_FOLDER_PATH2);
+            
+            char *oldDir; 
+            oldDir = searchOldDir(ROOT_FOLDER_PATH2);
+            
+            printf("##### 삭제대상 디렉토리명 :: %s\n", oldDir);
+
+            printf("##### 드라이브 내 용량이 부족하여 삭제 프로세스를 수행합니다\n");
+            printf("##### 삭제 대상 폴더경로 :: %s\n", oldDir);
+            
+            rmdirs(oldDir, 1);
+
+            printf("##### %s 하위 파일 및 폴더삭제를 완료 하였습니다.\n", oldDir);
+        }
+        else
+            printf("현재 마운트 드라이브의 유휴공간이 10 %% 이상입니다!!\n");
+
+
+        sleep(10);
+    }
+}
